b28bd53d93a1b3ae3f0b95ae834aeee018023285
wispae <wilferspaepen@gmail.com>
2025-08-18 00:24:30 +0200

    Initial version - Multithreading and optimizations WIP

---
+++ AttributeRenderingLibrary/CollectibleBehavior/CollectibleBehaviorGenerateCreativeStacks.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vintagestory.API.Common;
+using Vintagestory.API.Datastructures;
+using Vintagestory.ServerMods;
+using Vintagestory.ServerMods.NoObf;
+
+namespace AttributeRenderingLibrary;
+
+public class CollectibleBehaviorGenerateCreativeStacks : CollectibleBehavior
+{
+    public RegistryObjectVariantGroup[] AttributeVariantGroups { get; protected set; }
+    public List<string> AllowedCombinations { get; protected set; } = null;
+    public List<string> SkipCombinations { get; protected set; } = null;
+    public Dictionary<string, string[]> CreativeInventory { get; protected set; } = null;
+
+    public CollectibleBehaviorGenerateCreativeStacks(CollectibleObject collObj) : base(collObj) { }
+
+    public override void Initialize(JsonObject properties)
+    {
+        base.Initialize(properties);
+
+        if (properties != null)
+        {
+            AttributeVariantGroups = properties["variantgroups"].AsObject<RegistryObjectVariantGroup[]>(defaultValue: null);
+            CreativeInventory = properties["creativeinventory"].AsObject<Dictionary<string, string[]>>(defaultValue: new());
+        }
+    }
+}
--- AttributeRenderingLibrary/Systems/Core.cs
+++ AttributeRenderingLibrary/Systems/Core.cs
@@ -1,5 +1,8 @@
-﻿using HarmonyLib;
+﻿using AttributeRenderingLibrary.Utility.Creativestacks;
+using HarmonyLib;
+using System.Collections.Generic;
 using Vintagestory.API.Common;
+using Vintagestory.API.Server;
 
 namespace AttributeRenderingLibrary;
 
@@ -19,11 +22,30 @@ public class Core : ModSystem
         api.RegisterCollectibleBehaviorClass("AttributeRenderingLibrary.ContainedTransform", typeof(CollectibleBehaviorContainedTransform));
         api.RegisterCollectibleBehaviorClass("AttributeRenderingLibrary.AttachableToEntityTyped", typeof(CollectibleBehaviorAttachableToEntityTyped));
         api.RegisterCollectibleBehaviorClass("AttributeRenderingLibrary.HeldBagTyped", typeof(CollectibleBehaviorHeldBagTyped));
+        api.RegisterCollectibleBehaviorClass("AttributeRenderingLibrary.GenerateCreativeStacks", typeof(CollectibleBehaviorGenerateCreativeStacks));
         Mod.Logger.Event("started '{0}' mod", Mod.Info.Name);
     }
 
     public override void Dispose()
     {
         HarmonyInstance.UnpatchAll(HarmonyInstance.Id);
+    }
+
+    public override void AssetsFinalize(ICoreAPI api)
+    {
+        base.AssetsFinalize(api);
+
+        if (api.Side.IsServer())
+        {
+            CreateCreativeStacks(api as ICoreServerAPI);
+        }
+    }
+
+    private void CreateCreativeStacks(ICoreServerAPI sapi)
+    {
+        VariantLoader loader = new(sapi);
+        loader.CollectCollectibleObjectsToGenerate();
+        loader.CollectVariantsFromWorldProperties();
+        loader.ComposeVariants();
     }
 }
