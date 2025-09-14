7456ecda37d9c4f2fc4d692c85b78aac6aecea06
Dana <69315569+Craluminum2413@users.noreply.github.com>
2025-09-14 19:20:09 +0300

    added NWOrientable behavior that supports Variants

--- AttributeRenderingLibrary/Systems/Core.cs
+++ AttributeRenderingLibrary/Systems/Core.cs
@@ -15,12 +15,16 @@ public class Core : ModSystem
     public override void Start(ICoreAPI api)
     {
         api.RegisterItemClass("AttributeRenderingLibrary.ItemShapeTexturesFromAttributes", typeof(ItemShapeTexturesFromAttributes));
+
         api.RegisterCollectibleBehaviorClass("AttributeRenderingLibrary.ShapeTexturesFromAttributes", typeof(CollectibleBehaviorShapeTexturesFromAttributes));
         api.RegisterCollectibleBehaviorClass("AttributeRenderingLibrary.ContainedTransform", typeof(CollectibleBehaviorContainedTransform));
         api.RegisterCollectibleBehaviorClass("AttributeRenderingLibrary.HeldBagTyped", typeof(CollectibleBehaviorHeldBagTyped));
+
         api.RegisterBlockBehaviorClass("AttributeRenderingLibrary.BlockShapeTexturesFromAttributes", typeof(BlockBehaviorShapeTexturesFromAttributes));
         api.RegisterBlockBehaviorClass("AttributeRenderingLibrary.HorizontalOrientable", typeof(AttributeRenderingLibrary.BlockBehaviorHorizontalOrientable));
         api.RegisterBlockBehaviorClass("AttributeRenderingLibrary.HorizontalAttachable", typeof(AttributeRenderingLibrary.BlockBehaviorHorizontalAttachable));
+        api.RegisterBlockBehaviorClass("AttributeRenderingLibrary.NWOrientable", typeof(AttributeRenderingLibrary.BlockBehaviorNWOrientable));
+
         api.RegisterBlockEntityBehaviorClass("AttributeRenderingLibrary.ShapeTexturesFromAttributes", typeof(BlockEntityBehaviorShapeTexturesFromAttributes));
         Mod.Logger.Event("started '{0}' mod", Mod.Info.Name);
     }
