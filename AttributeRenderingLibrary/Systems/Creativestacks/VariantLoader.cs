375bedfbb96e673c094b5efdd53284d93cba3cfa
Muppers <muppers@users.noreply.github.com>
2026-07-31 21:52:52 +0300

    Change modid to attributerenderinglibraryfork for standalone fork release

--- AttributeRenderingLibrary/Systems/Creativestacks/VariantLoader.cs
+++ AttributeRenderingLibrary/Systems/Creativestacks/VariantLoader.cs
@@ -482,9 +482,9 @@ public class VariantLoader : ModSystem
             // resolve seems to only perform read operations on WorldAccessor,
             // so this should be fine to do off the main thread I think
 #if DEBUG
-            result[i].Resolve(serverApi.World, "attributerenderinglibrary");
+            result[i].Resolve(serverApi.World, "attributerenderinglibraryfork");
 #else
-            result[i].Resolve(serverApi.World, "attributerenderinglibrary", false);
+            result[i].Resolve(serverApi.World, "attributerenderinglibraryfork", false);
 #endif
         }
 
